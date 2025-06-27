eb8829945dafd9f1600e45424b6224f8eaf1bb78
harshittt-21 <harshitrawat2102@gmail.com>
2025-06-27 04:28:11 +0000

    FUNCTIONS,ETC

---
+++ basics/introto.js
@@ -0,0 +1,52 @@
+// FUNCTIONS :
+
+// function sumOfTwo(param1,param2){
+//     var answer = param1 + param2;
+
+//     return answer;
+// }
+
+
+
+// var arg1 =10;
+// var arg2 =20;
+// console.log(sumOfTwo(arg1,arg2));
+// var x =300;
+
+// function firstFun(){
+//     var a =20;
+//     var b =30;
+//             console.log("HELLO I AM FIRST FUNCTION");
+
+//     function secFun(){
+//         var c = 50;
+//             console.log("HELLO I AM SECOND FUNCTION");
+//         function thirdFun(){
+//             var d =80;
+//             console.log("HELLO I AM THIRD FUNCTION");
+//             console.log(a+b+c+d+x);
+//         }
+//         thirdFun();
+//     }
+//     secFun();
+// }
+// console.log(firstFun());
+
+
+// setTimeout & setInterval
+
+// setTimeout(function (){
+//     console.log("HELLO I AM SETTIMEOUT")
+// },5000);
+
+
+// setInterval(function (){
+//     console.log("HELLOO I AM SETINTERVAL")
+// },2000);
+
+
+// for (let i =0; i<=5;i++){
+//     setTimeout(function (){
+//         console.log(i)
+//     },2000);
+// }
